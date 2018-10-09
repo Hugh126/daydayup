0bd47c85295735f98bf9fdbb7f7161ae7f7765a2
hugh <283341330@qq.com>
2018-10-09 17:07:34 +0800

    [Add] 新增练习单个类文件 [Delete] 删除IDEA 工程文件

---
+++ src/main/java/util/IntUtil.java
@@ -0,0 +1,37 @@
+package util;
+
+/**
+ * @author hugh
+ * @Title: IntUtil
+ * @ProjectName springboottest
+ * @Description: TODO
+ * @date 2018/10/811:38
+ */
+public class IntUtil {
+
+    /**
+     * 两个整数通过异或实现互换
+     * @param a
+     * @param b
+     */
+    public static void exchangeInt(int a, int b) {
+            if(a == b) return; //防止&a，&b指向同一个地址；那样结果会错误。
+            a ^= b;
+            b ^= a;
+            a ^= b;
+    }
+
+    /**
+     * 求整数二进制表示中1的个数
+     * @param n
+     * @return
+     */
+    public static int get1LengthOfInt(int n) {
+        int num = 0;
+        while (n != 0) {
+            n &= n-1;
+            num ++;
+        }
+        return num;
+    }
+}
