224cf49db6577ded8db097225398e2fe50a96e2b
hugh <283341330@qq.com>
2018-10-09 17:37:10 +0800

    [Modify] 新增springboot两种参数获取方式

--- src/main/java/com/example/myspring/App.java
+++ src/main/java/com/example/myspring/App.java
@@ -2,16 +2,39 @@ package com.example.myspring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @EnableAutoConfiguration
 public class App 
 {
-    @RequestMapping("/hello")
+    @RequestMapping("/hi")
     String home() {
-        return "Hello World!";
+        return "Hi World!";
+    }
+
+    /**
+     * 使用PathVariable 接受参数
+     * @param userName
+     * @return
+     */
+    @RequestMapping("/hello/{username}")
+    String sayHello(@PathVariable("username") String userName) {
+        return "Hello  " + userName;
+    }
+
+    /**
+     * 使用RequestParam获取参数
+     * 对于URL请求方式为http://localhost:8080/hehe?username=qq
+     * @param userName
+     * @return
+     */
+    @RequestMapping("/hehe")
+    String sayHello2(@RequestParam("username") String userName) {
+        return "hehe  " + userName;
     }
 
     public static void main( String[] args )
